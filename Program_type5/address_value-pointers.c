70f283f76b2b31778c2f549bb148b033c0541c3b
Sakshi Hanumant Meshram <meshramsakshi227@gmail.com>
2026-06-23 22:34:23 +0530

    Delete Program_type5 directory

--- Program_type5/address_value-pointers.c
+++
@@ -1,9 +0,0 @@
-//Write a program to demonstrate the concept of pointers by printing the address and value of a variable.
-#include <stdio.h>
-int main()
-{
-    int a = 10;
-    printf("Value of a = %d\n",a);
-    printf("Address of a = %p\n",&a);
-    return 0;
-}
